618220451afdb238432d0323f98b6e9b663086a4
quanpn90 <quanpn90@gmail.com>
2017-01-23 14:39:06 +0100

    Convert code to cudnn - several times faster

--- sample.lua
+++ sample.lua
@@ -23,7 +23,7 @@ cmd:text('Sample from a character-level language model')
 cmd:text()
 cmd:text('Options')
 -- required:
-cmd:argument('-model','model checkpoint to use for sampling')
+cmd:option('-model', 'cv/lm_lstm_epoch50.00_1.3898.t7', 'model checkpoint to use for sampling')
 -- optional parameters
 cmd:option('-seed',123,'random number generator\'s seed')
 cmd:option('-sample',1,' 0 to use max at each timestep, 1 to sample at each timestep')
@@ -31,7 +31,6 @@ cmd:option('-primetext',"",'used as a prompt to "seed" the state of the LSTM usi
 cmd:option('-length',2000,'number of characters to sample')
 cmd:option('-temperature',1,'temperature of sampling')
 cmd:option('-gpuid',0,'which gpu to use. -1 = use CPU')
-cmd:option('-opencl',0,'use OpenCL (instead of CUDA)')
 cmd:option('-verbose',1,'set to 0 to ONLY print the sampled text, no diagnostics')
 cmd:text()
 
@@ -44,7 +43,7 @@ function gprint(str)
 end
 
 -- check that cunn/cutorch are installed if user wants to use the GPU
-if opt.gpuid >= 0 and opt.opencl == 0 then
+if opt.gpuid >= 0 then
     local ok, cunn = pcall(require, 'cunn')
     local ok2, cutorch = pcall(require, 'cutorch')
     if not ok then gprint('package cunn not found!') end
@@ -58,24 +57,11 @@ if opt.gpuid >= 0 and opt.opencl == 0 then
         gprint('Falling back on CPU mode')
         opt.gpuid = -1 -- overwrite user setting
     end
+    
+    require 'cudnn'
+    require 'rnn'
 end
 
--- check that clnn/cltorch are installed if user wants to use OpenCL
-if opt.gpuid >= 0 and opt.opencl == 1 then
-    local ok, cunn = pcall(require, 'clnn')
-    local ok2, cutorch = pcall(require, 'cltorch')
-    if not ok then print('package clnn not found!') end
-    if not ok2 then print('package cltorch not found!') end
-    if ok and ok2 then
-        gprint('using OpenCL on GPU ' .. opt.gpuid .. '...')
-        gprint('Make sure that your saved checkpoint was also trained with GPU. If it was trained with CPU use -gpuid -1 for sampling as well')
-        cltorch.setDevice(opt.gpuid + 1) -- note +1 to make it 0 indexed! sigh lua
-        torch.manualSeed(opt.seed)
-    else
-        gprint('Falling back on CPU mode')
-        opt.gpuid = -1 -- overwrite user setting
-    end
-end
 
 torch.manualSeed(opt.seed)
 
@@ -84,8 +70,6 @@ if not lfs.attributes(opt.model, 'mode') then
     gprint('Error: File ' .. opt.model .. ' does not exist. Are you sure you didn\'t forget to prepend cv/ ?')
 end
 checkpoint = torch.load(opt.model)
-protos = checkpoint.protos
-protos.rnn:evaluate() -- put in eval mode so that dropout works properly
 
 -- initialize the vocabulary (and its inverted version)
 local vocab = checkpoint.vocab
@@ -94,43 +78,29 @@ for c,i in pairs(vocab) do ivocab[i] = c end
 
 -- initialize the rnn state to all zeros
 gprint('creating an ' .. checkpoint.opt.model .. '...')
-local current_state
-current_state = {}
-for L = 1,checkpoint.opt.num_layers do
-    -- c and h for all layers
-    local h_init = torch.zeros(1, checkpoint.opt.rnn_size):double()
-    if opt.gpuid >= 0 and opt.opencl == 0 then h_init = h_init:cuda() end
-    if opt.gpuid >= 0 and opt.opencl == 1 then h_init = h_init:cl() end
-    table.insert(current_state, h_init:clone())
-    if checkpoint.opt.model == 'lstm' then
-        table.insert(current_state, h_init:clone())
-    end
-end
-state_size = #current_state
+local net = checkpoint.net
+net:evaluate()
+
 
 -- do a few seeded timesteps
 local seed_text = opt.primetext
+local prediction
 if string.len(seed_text) > 0 then
     gprint('seeding with ' .. seed_text)
     gprint('--------------------------')
     for c in seed_text:gmatch'.' do
         prev_char = torch.Tensor{vocab[c]}
         io.write(ivocab[prev_char[1]])
-        if opt.gpuid >= 0 and opt.opencl == 0 then prev_char = prev_char:cuda() end
-        if opt.gpuid >= 0 and opt.opencl == 1 then prev_char = prev_char:cl() end
-        local lst = protos.rnn:forward{prev_char, unpack(current_state)}
-        -- lst is a list of [state1,state2,..stateN,output]. We want everything but last piece
-        current_state = {}
-        for i=1,state_size do table.insert(current_state, lst[i]) end
-        prediction = lst[#lst] -- last element holds the log probabilities
+        if opt.gpuid >= 0 then prev_char = prev_char:cuda() end
+        prediction = protos.rnn:forward(prev_char)
+       
     end
 else
     -- fill with uniform probabilities over characters (? hmm)
     gprint('missing seed text, using uniform probability over first character')
     gprint('--------------------------')
     prediction = torch.Tensor(1, #ivocab):fill(1)/(#ivocab)
-    if opt.gpuid >= 0 and opt.opencl == 0 then prediction = prediction:cuda() end
-    if opt.gpuid >= 0 and opt.opencl == 1 then prediction = prediction:cl() end
+    if opt.gpuid >= 0 then prediction = prediction:cuda() end
 end
 
 -- start sampling/argmaxing
@@ -148,14 +118,14 @@ for i=1, opt.length do
         probs:div(torch.sum(probs)) -- renormalize so probs sum to one
         prev_char = torch.multinomial(probs:float(), 1):resize(1):float()
     end
-
+		
+		prev_char:resize(1, 1)
+		prev_char = prev_char:cuda()
+    --~ print(prev_char)
     -- forward the rnn for next character
-    local lst = protos.rnn:forward{prev_char, unpack(current_state)}
-    current_state = {}
-    for i=1,state_size do table.insert(current_state, lst[i]) end
-    prediction = lst[#lst] -- last element holds the log probabilities
+		prediction = net:forward(prev_char)
 
-    io.write(ivocab[prev_char[1]])
+    io.write(ivocab[prev_char[1][1]])
 end
 io.write('\n') io.flush()
 
--- train.lua
+++ train.lua
@@ -18,6 +18,7 @@ require 'nn'
 require 'nngraph'
 require 'optim'
 require 'lfs'
+require 'rnn'
 
 require 'util.OneHot'
 require 'util.misc'
@@ -35,17 +36,17 @@ cmd:text('Options')
 -- data
 cmd:option('-data_dir','data/tinyshakespeare','data directory. Should contain the file input.txt with input data')
 -- model params
-cmd:option('-rnn_size', 128, 'size of LSTM internal state')
-cmd:option('-num_layers', 2, 'number of layers in the LSTM')
+cmd:option('-rnn_size', 512, 'size of LSTM internal state')
+cmd:option('-num_layers', 3, 'number of layers in the LSTM')
 cmd:option('-model', 'lstm', 'lstm,gru or rnn')
 -- optimization
 cmd:option('-learning_rate',2e-3,'learning rate')
 cmd:option('-learning_rate_decay',0.97,'learning rate decay')
 cmd:option('-learning_rate_decay_after',10,'in number of epochs, when to start decaying the learning rate')
 cmd:option('-decay_rate',0.95,'decay rate for rmsprop')
-cmd:option('-dropout',0,'dropout for regularization, used after each RNN hidden layer. 0 = no dropout')
-cmd:option('-seq_length',50,'number of timesteps to unroll for')
-cmd:option('-batch_size',50,'number of sequences to train on in parallel')
+cmd:option('-dropout',0.25,'dropout for regularization, used after each RNN hidden layer. 0 = no dropout')
+cmd:option('-seq_length',64,'number of timesteps to unroll for')
+cmd:option('-batch_size',64,'number of sequences to train on in parallel')
 cmd:option('-max_epochs',50,'number of full passes through the training data')
 cmd:option('-grad_clip',5,'clip gradients at this value')
 cmd:option('-train_frac',0.95,'fraction of data that goes into train set')
@@ -54,14 +55,13 @@ cmd:option('-val_frac',0.05,'fraction of data that goes into validation set')
 cmd:option('-init_from', '', 'initialize network parameters from checkpoint at this path')
 -- bookkeeping
 cmd:option('-seed',123,'torch manual random number generator seed')
-cmd:option('-print_every',1,'how many steps/minibatches between printing out the loss')
+cmd:option('-print_every',1000,'how many steps/minibatches between printing out the loss')
 cmd:option('-eval_val_every',1000,'every how many iterations should we evaluate on validation data?')
 cmd:option('-checkpoint_dir', 'cv', 'output directory where checkpoints get written')
 cmd:option('-savefile','lstm','filename to autosave the checkpont to. Will be inside checkpoint_dir/')
 cmd:option('-accurate_gpu_timing',0,'set this flag to 1 to get precise timings when using GPU. Might make code bit slower but reports accurate timings.')
 -- GPU/CPU
-cmd:option('-gpuid',0,'which gpu to use. -1 = use CPU')
-cmd:option('-opencl',0,'use OpenCL (instead of CUDA)')
+cmd:option('-gpuid',1,'which gpu to use. -1 = use CPU')
 cmd:text()
 
 -- parse input params
@@ -72,15 +72,16 @@ local test_frac = math.max(0, 1 - (opt.train_frac + opt.val_frac))
 local split_sizes = {opt.train_frac, opt.val_frac, test_frac} 
 
 -- initialize cunn/cutorch for training on the GPU and fall back to CPU gracefully
-if opt.gpuid >= 0 and opt.opencl == 0 then
+if opt.gpuid >= 1 then
     local ok, cunn = pcall(require, 'cunn')
     local ok2, cutorch = pcall(require, 'cutorch')
     if not ok then print('package cunn not found!') end
     if not ok2 then print('package cutorch not found!') end
     if ok and ok2 then
         print('using CUDA on GPU ' .. opt.gpuid .. '...')
-        cutorch.setDevice(opt.gpuid + 1) -- note +1 to make it 0 indexed! sigh lua
+        cutorch.setDevice(opt.gpuid) -- note +1 to make it 0 indexed! sigh lua
         cutorch.manualSeed(opt.seed)
+        require 'cudnn'
     else
         print('If cutorch and cunn are installed, your CUDA toolkit may be improperly configured.')
         print('Check your CUDA toolkit installation, rebuild cutorch and cunn, and try again.')
@@ -89,23 +90,6 @@ if opt.gpuid >= 0 and opt.opencl == 0 then
     end
 end
 
--- initialize clnn/cltorch for training on the GPU and fall back to CPU gracefully
-if opt.gpuid >= 0 and opt.opencl == 1 then
-    local ok, cunn = pcall(require, 'clnn')
-    local ok2, cutorch = pcall(require, 'cltorch')
-    if not ok then print('package clnn not found!') end
-    if not ok2 then print('package cltorch not found!') end
-    if ok and ok2 then
-        print('using OpenCL on GPU ' .. opt.gpuid .. '...')
-        cltorch.setDevice(opt.gpuid + 1) -- note +1 to make it 0 indexed! sigh lua
-        torch.manualSeed(opt.seed)
-    else
-        print('If cltorch and clnn are installed, your OpenCL driver may be improperly configured.')
-        print('Check your OpenCL driver installation, check output of clinfo command, and try again.')
-        print('Falling back on CPU mode')
-        opt.gpuid = -1 -- overwrite user setting
-    end
-end
 
 -- create the data loader class
 local loader = CharSplitLMMinibatchLoader.create(opt.data_dir, opt.batch_size, opt.seq_length, split_sizes)
@@ -115,12 +99,23 @@ print('vocab size: ' .. vocab_size)
 -- make sure output directory exists
 if not path.exists(opt.checkpoint_dir) then lfs.mkdir(opt.checkpoint_dir) end
 
+
+
 -- define the model: prototypes for one timestep, then clone them in time
-local do_random_init = true
+--~ local do_random_init = true
+
+local net, criterion
+
+
+
+
+
+
+
 if string.len(opt.init_from) > 0 then
     print('loading a model from checkpoint ' .. opt.init_from)
     local checkpoint = torch.load(opt.init_from)
-    protos = checkpoint.protos
+    net = checkpoint.net
     -- make sure the vocabs are the same
     local vocab_compatible = true
     local checkpoint_vocab_size = 0
@@ -143,64 +138,64 @@ if string.len(opt.init_from) > 0 then
     do_random_init = false
 else
     print('creating an ' .. opt.model .. ' with ' .. opt.num_layers .. ' layers')
-    protos = {}
-    if opt.model == 'lstm' then
-        protos.rnn = LSTM.lstm(vocab_size, opt.rnn_size, opt.num_layers, opt.dropout)
-    elseif opt.model == 'gru' then
-        protos.rnn = GRU.gru(vocab_size, opt.rnn_size, opt.num_layers, opt.dropout)
-    elseif opt.model == 'rnn' then
-        protos.rnn = RNN.rnn(vocab_size, opt.rnn_size, opt.num_layers, opt.dropout)
-    end
-    protos.criterion = nn.ClassNLLCriterion()
+    net = nn.Sequential()
+		net:add(nn.LookupTable(vocab_size, opt.rnn_size))
+		
+		
+		local rnn = cudnn.LSTM(opt.rnn_size, opt.rnn_size, opt.num_layers, false, opt.dropout, true)
+
+		net:add(rnn)
+
+		local linear = nn.Linear(opt.rnn_size, vocab_size)
+
+		net:add(nn.Sequencer(linear))
+		net:add(nn.Sequencer(cudnn.LogSoftMax()))
+
+		
 end
 
--- the initial state of the cell/hidden states
-init_state = {}
-for L=1,opt.num_layers do
-    local h_init = torch.zeros(opt.batch_size, opt.rnn_size)
-    if opt.gpuid >=0 and opt.opencl == 0 then h_init = h_init:cuda() end
-    if opt.gpuid >=0 and opt.opencl == 1 then h_init = h_init:cl() end
-    table.insert(init_state, h_init:clone())
-    if opt.model == 'lstm' then
-        table.insert(init_state, h_init:clone())
-    end
-end
+local crit = nn.ClassNLLCriterion()
+criterion = nn.SequencerCriterion(crit) 
+local rnn = net.modules[2] -- a pointer to the rnn
 
--- ship the model to the GPU if desired
-if opt.gpuid >= 0 and opt.opencl == 0 then
-    for k,v in pairs(protos) do v:cuda() end
-end
-if opt.gpuid >= 0 and opt.opencl == 1 then
-    for k,v in pairs(protos) do v:cl() end
-end
+
+net:cuda()
+criterion:cuda()
 
 -- put the above things into one flattened parameters tensor
-params, grad_params = model_utils.combine_all_parameters(protos.rnn)
+params, grad_params = net:getParameters()
+params:uniform(-0.08, 0.08)
+
+local trainState = {}
+
+trainState.hiddenOutput = torch.CudaTensor()
+trainState.hiddenCell = torch.CudaTensor()
 
 -- initialization
-if do_random_init then
-    params:uniform(-0.08, 0.08) -- small uniform numbers
-end
--- initialize the LSTM forget gates with slightly higher biases to encourage remembering in the beginning
-if opt.model == 'lstm' then
-    for layer_idx = 1, opt.num_layers do
-        for _,node in ipairs(protos.rnn.forwardnodes) do
-            if node.data.annotations.name == "i2h_" .. layer_idx then
-                print('setting forget gate biases to 1 in LSTM layer ' .. layer_idx)
-                -- the gates are, in order, i,f,o,g, so f is the 2nd block of weights
-                node.data.module.bias[{{opt.rnn_size+1, 2*opt.rnn_size}}]:fill(1.0)
-            end
-        end
-    end
-end
+--~ if do_random_init then
+    --~ params:uniform(-0.08, 0.08) -- small uniform numbers
+--~ end
+--~ -- initialize the LSTM forget gates with slightly higher biases to encourage remembering in the beginning
+--~ if opt.model == 'lstm' then
+    --~ for layer_idx = 1, opt.num_layers do
+        --~ for _,node in ipairs(protos.rnn.forwardnodes) do
+            --~ if node.data.annotations.name == "i2h_" .. layer_idx then
+                --~ print('setting forget gate biases to 1 in LSTM layer ' .. layer_idx)
+                --~ -- the gates are, in order, i,f,o,g, so f is the 2nd block of weights
+                --~ node.data.module.bias[{{opt.rnn_size+1, 2*opt.rnn_size}}]:fill(1.0)
+            --~ end
+        --~ end
+    --~ end
+--~ end
 
 print('number of parameters in the model: ' .. params:nElement())
--- make a bunch of clones after flattening, as that reallocates memory
-clones = {}
-for name,proto in pairs(protos) do
-    print('cloning ' .. name)
-    clones[name] = model_utils.clone_many_times(proto, opt.seq_length, not proto.parameters)
-end
+
+--~ -- make a bunch of clones after flattening, as that reallocates memory
+--~ clones = {}
+--~ for name,proto in pairs(protos) do
+    --~ print('cloning ' .. name)
+    --~ clones[name] = model_utils.clone_many_times(proto, opt.seq_length, not proto.parameters)
+--~ end
 
 -- preprocessing helper function
 function prepro(x,y)
@@ -211,47 +206,56 @@ function prepro(x,y)
         x = x:float():cuda()
         y = y:float():cuda()
     end
-    if opt.gpuid >= 0 and opt.opencl == 1 then -- ship the input arrays to GPU
-        x = x:cl()
-        y = y:cl()
-    end
+
     return x,y
 end
 
 -- evaluate the loss over an entire split
 function eval_split(split_index, max_batches)
+		
+		
+		net:evaluate()
+		
+		trainState.hiddenOutput = rnn.hiddenOutput:clone()
+		trainState.cellOutput = rnn.cellOutput:clone()
+		
+		rnn.hiddenOutput:zero()
+		rnn.cellOutput:zero()
+		
     print('evaluating loss over split index ' .. split_index)
     local n = loader.split_sizes[split_index]
     if max_batches ~= nil then n = math.min(max_batches, n) end
 
     loader:reset_batch_pointer(split_index) -- move batch iteration pointer for this split to front
     local loss = 0
-    local rnn_state = {[0] = init_state}
     
     for i = 1,n do -- iterate over batches in the split
-        -- fetch a batch
+        --~ -- fetch a batch
         local x, y = loader:next_batch(split_index)
         x,y = prepro(x,y)
-        -- forward pass
-        for t=1,opt.seq_length do
-            clones.rnn[t]:evaluate() -- for dropout proper functioning
-            local lst = clones.rnn[t]:forward{x[t], unpack(rnn_state[t-1])}
-            rnn_state[t] = {}
-            for i=1,#init_state do table.insert(rnn_state[t], lst[i]) end
-            prediction = lst[#lst] 
-            loss = loss + clones.criterion[t]:forward(prediction, y[t])
-        end
-        -- carry over lstm state
-        rnn_state[0] = rnn_state[#rnn_state]
+        
+        local net_output = net:forward(x)
+        
+        loss = loss + criterion:forward(net_output, y)
         print(i .. '/' .. n .. '...')
     end
 
     loss = loss / opt.seq_length / n
+    
+    -- back to the current train state again
+    rnn.hiddenOutput:resize(trainState.hiddenOutput:size()):copy(trainState.hiddenOutput)
+    rnn.cellOutput:resize(trainState.cellOutput:size()):copy(trainState.cellOutput)
+    net:training()
+    
     return loss
 end
 
+local val_loss = eval_split(2) 
+
+print(val_loss)
+
 -- do fwd/bwd and return loss, grad_params
-local init_state_global = clone_list(init_state)
+--~ local init_state_global = clone_list(init_state)
 function feval(x)
     if x ~= params then
         params:copy(x)
@@ -262,43 +266,62 @@ function feval(x)
     local x, y = loader:next_batch(1)
     x,y = prepro(x,y)
     ------------------- forward pass -------------------
-    local rnn_state = {[0] = init_state_global}
-    local predictions = {}           -- softmax outputs
-    local loss = 0
-    for t=1,opt.seq_length do
-        clones.rnn[t]:training() -- make sure we are in correct mode (this is cheap, sets flag)
-        local lst = clones.rnn[t]:forward{x[t], unpack(rnn_state[t-1])}
-        rnn_state[t] = {}
-        for i=1,#init_state do table.insert(rnn_state[t], lst[i]) end -- extract the state, without output
-        predictions[t] = lst[#lst] -- last element is the prediction
-        loss = loss + clones.criterion[t]:forward(predictions[t], y[t])
-    end
+   
+    
+    local net_output = net:forward(x)
+    
+    local loss = criterion:forward(net_output, y)
+    
     loss = loss / opt.seq_length
-    ------------------ backward pass -------------------
-    -- initialize gradient at time t to be zeros (there's no influence from future)
-    local drnn_state = {[opt.seq_length] = clone_list(init_state, true)} -- true also zeros the clones
-    for t=opt.seq_length,1,-1 do
-        -- backprop through loss, and softmax/linear
-        local doutput_t = clones.criterion[t]:backward(predictions[t], y[t])
-        table.insert(drnn_state[t], doutput_t)
-        local dlst = clones.rnn[t]:backward({x[t], unpack(rnn_state[t-1])}, drnn_state[t])
-        drnn_state[t-1] = {}
-        for k,v in pairs(dlst) do
-            if k > 1 then -- k == 1 is gradient on x, which we dont need
-                -- note we do k-1 because first item is dembeddings, and then follow the 
-                -- derivatives of the state, starting at index 2. I know...
-                drnn_state[t-1][k-1] = v
-            end
-        end
-    end
-    ------------------------ misc ----------------------
-    -- transfer final state to initial state (BPTT)
-    init_state_global = rnn_state[#rnn_state] -- NOTE: I don't think this needs to be a clone, right?
-    -- grad_params:div(opt.seq_length) -- this line should be here but since we use rmsprop it would have no effect. Removing for efficiency
-    -- clip gradient element-wise
+    
+    local dloss = criterion:backward(net_output, y)
+    
+    net:backward(x, dloss)
+    
     grad_params:clamp(-opt.grad_clip, opt.grad_clip)
+    
     return loss, grad_params
+    
 end
+    
+    
+    --~ local rnn_state = {[0] = init_state_global}
+    --~ local predictions = {}           -- softmax outputs
+    --~ local loss = 0
+    --~ for t=1,opt.seq_length do
+        --~ clones.rnn[t]:training() -- make sure we are in correct mode (this is cheap, sets flag)
+        --~ local lst = clones.rnn[t]:forward{x[t], unpack(rnn_state[t-1])}
+        --~ rnn_state[t] = {}
+        --~ for i=1,#init_state do table.insert(rnn_state[t], lst[i]) end -- extract the state, without output
+        --~ predictions[t] = lst[#lst] -- last element is the prediction
+        --~ loss = loss + clones.criterion[t]:forward(predictions[t], y[t])
+    --~ end
+    --~ loss = loss / opt.seq_length
+    --~ ------------------ backward pass -------------------
+    --~ -- initialize gradient at time t to be zeros (there's no influence from future)
+    --~ local drnn_state = {[opt.seq_length] = clone_list(init_state, true)} -- true also zeros the clones
+    --~ for t=opt.seq_length,1,-1 do
+        --~ -- backprop through loss, and softmax/linear
+        --~ local doutput_t = clones.criterion[t]:backward(predictions[t], y[t])
+        --~ table.insert(drnn_state[t], doutput_t)
+        --~ local dlst = clones.rnn[t]:backward({x[t], unpack(rnn_state[t-1])}, drnn_state[t])
+        --~ drnn_state[t-1] = {}
+        --~ for k,v in pairs(dlst) do
+            --~ if k > 1 then -- k == 1 is gradient on x, which we dont need
+                --~ -- note we do k-1 because first item is dembeddings, and then follow the 
+                --~ -- derivatives of the state, starting at index 2. I know...
+                --~ drnn_state[t-1][k-1] = v
+            --~ end
+        --~ end
+    --~ end
+    --~ ------------------------ misc ----------------------
+    --~ -- transfer final state to initial state (BPTT)
+    --~ init_state_global = rnn_state[#rnn_state] -- NOTE: I don't think this needs to be a clone, right?
+    --~ -- grad_params:div(opt.seq_length) -- this line should be here but since we use rmsprop it would have no effect. Removing for efficiency
+    --~ -- clip gradient element-wise
+    --~ grad_params:clamp(-opt.grad_clip, opt.grad_clip)
+    --~ return loss, grad_params
+--~ end
 
 -- start optimization here
 train_losses = {}
@@ -307,6 +330,7 @@ local optim_state = {learningRate = opt.learning_rate, alpha = opt.decay_rate}
 local iterations = opt.max_epochs * loader.ntrain
 local iterations_per_epoch = loader.ntrain
 local loss0 = nil
+net:training()
 for i = 1, iterations do
     local epoch = i / loader.ntrain
 
@@ -343,7 +367,7 @@ for i = 1, iterations do
         local savefile = string.format('%s/lm_%s_epoch%.2f_%.4f.t7', opt.checkpoint_dir, opt.savefile, epoch, val_loss)
         print('saving checkpoint to ' .. savefile)
         local checkpoint = {}
-        checkpoint.protos = protos
+        checkpoint.net = net
         checkpoint.opt = opt
         checkpoint.train_losses = train_losses
         checkpoint.val_loss = val_loss
